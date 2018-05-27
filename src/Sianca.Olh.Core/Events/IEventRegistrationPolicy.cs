5a8ddf008ec840ea1452c68f416b7c20cdfc498b
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-05-26 21:15:46 -0300

    entities for event

---
+++ src/Sianca.Olh.Core/Events/IEventRegistrationPolicy.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Services;
+using Sianca.Olh.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sianca.Olh.Events
+{
+    public interface IEventRegistrationPolicy : IDomainService
+    {
+        /// <summary>
+        /// Checks if given user can register to <see cref="@event"/> and throws exception if can not.
+        /// </summary>
+        Task CheckRegistrationAttemptAsync(Event @event, User user);
+    }
+}
