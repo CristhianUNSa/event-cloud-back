00562ad9ac763494be8a32b032bd6b2b417e6708
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-05-27 15:31:56 -0300

    EventRegistrationPolicy

--- src/Sianca.Olh.Core/Configuration/AppSettingProvider.cs
+++ src/Sianca.Olh.Core/Configuration/AppSettingProvider.cs
@@ -9,7 +9,8 @@ namespace Sianca.Olh.Configuration
         {
             return new[]
             {
-                new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true)
+                new SettingDefinition(AppSettingNames.UiTheme, "red", scopes: SettingScopes.Application | SettingScopes.Tenant | SettingScopes.User, isVisibleToClients: true),
+                new SettingDefinition(AppSettingNames.MaxAllowedEventRegistrationCountInLast30DaysPerUser, "45", scopes: SettingScopes.User, isVisibleToClients: true)
             };
         }
     }
