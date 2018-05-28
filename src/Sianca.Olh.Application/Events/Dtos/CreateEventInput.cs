45f20b7d21fefcff53662ea7c551eff14004454a
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-05-27 21:35:43 -0300

    Application Service

---
+++ src/Sianca.Olh.Application/Events/Dtos/CreateEventInput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sianca.Olh.Events.Dtos
+{
+    public class CreateEventInput
+    {
+        [Required]
+        [StringLength(Event.MaxTitleLength)]
+        public string Title { get; set; }
+
+        [StringLength(Event.MaxDescriptionLength)]
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int MaxRegistrationCount { get; set; }
+    }
+}
--- src/Sianca.Olh.EntityFrameworkCore/EntityFrameworkCore/OlhDbContext.cs
+++ src/Sianca.Olh.EntityFrameworkCore/EntityFrameworkCore/OlhDbContext.cs
@@ -3,13 +3,17 @@ using Abp.Zero.EntityFrameworkCore;
 using Sianca.Olh.Authorization.Roles;
 using Sianca.Olh.Authorization.Users;
 using Sianca.Olh.MultiTenancy;
+using Sianca.Olh.Events;
 
 namespace Sianca.Olh.EntityFrameworkCore
 {
     public class OlhDbContext : AbpZeroDbContext<Tenant, Role, User, OlhDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventRegistration> EventRegistrations { get; set; }
+
+
         public OlhDbContext(DbContextOptions<OlhDbContext> options)
             : base(options)
         {
