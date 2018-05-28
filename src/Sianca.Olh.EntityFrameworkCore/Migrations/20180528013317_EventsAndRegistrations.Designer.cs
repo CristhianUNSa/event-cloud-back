c184f64c93c186728f1a0840522a5e7429116bb2
Cristhian Valencia <valenciacristhian47@gmail.com>
2018-05-27 23:12:03 -0300

    migration

--- src/Sianca.Olh.EntityFrameworkCore/Migrations/20180528013317_EventsAndRegistrations.Designer.cs
+++ src/Sianca.Olh.EntityFrameworkCore/Migrations/20180528013317_EventsAndRegistrations.Designer.cs
@@ -16,9 +16,10 @@ using System;
 namespace Sianca.Olh.Migrations
 {
     [DbContext(typeof(OlhDbContext))]
-    partial class OlhDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180528013317_EventsAndRegistrations")]
+    partial class EventsAndRegistrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
